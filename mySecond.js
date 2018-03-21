44f527489e6302e27cc111b09bb72f600b29e984
Swati Marda <swati.marda@32908-MBPR.local>
2018-03-20 21:54:35 -0700

    My site ready for deployment.

--- mySecond.js
+++ mySecond.js
@@ -4,6 +4,15 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/public')); 
 app.set('port', process.env.PORT || 8080);
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('Main.html');
+});
+
 app.get('/Main*' ,   function(req,res,next) {
     res.sendfile('views/Main.html');
 } );
