979961b218cc308b25dc84378e61ff0092f7831d
Swati Marda <swati.marda@32908-MBPR.local>
2018-03-20 21:59:18 -0700

    My site ready for deployment.

--- mySecond.js
+++ mySecond.js
@@ -13,6 +13,18 @@ app.get('/', function(req, res) {
     res.render('Main.html');
 });
 
+/*Allow CORS*/
+app.use(function(req, res, next) {
+     
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+      
+    next();
+});
+
 app.get('/Main*' ,   function(req,res,next) {
     res.sendfile('views/Main.html');
 } );
